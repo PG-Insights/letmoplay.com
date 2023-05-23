bbfb759c908fe242790ce5168b873dec6625a18d
quaaalud <dludwins@outlook.com>
2023-05-22 22:38:54 -0500

    updated entrants

--- backend/schemas/giveaway_entrants.py
+++ backend/schemas/giveaway_entrants.py
@@ -4,10 +4,7 @@ from pydantic import BaseModel, EmailStr
 
 
 class EntrantCreate(BaseModel):
-    name: str
     email : EmailStr
-    zip_code: str
-    agree_tos: bool
     
 
 class ShowEntrant(BaseModel):
