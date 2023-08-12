c0d31ce9e703848934ba596e28b51a8965452dac
Dale <dludwins@outlook.com>
2023-08-12 18:28:58 -0400

    updated filename

--- backend/apis/version1/email/send_message_received_email.py
+++ backend/apis/version1/email/send_message_received_email.py
@@ -12,7 +12,7 @@ from email_app.main import transfer_files_to_remote_and_send_email
 def send_message_received_email(email_str: str) -> None:
     dir_path = Path(Path(__file__).parent, 'email_app')
     transfer_files_to_remote_and_send_email(
-        str(Path(dir_path, 'html_files', 'Your Message Has Been Sent to Let Mo Play.html')),
+        str(Path(dir_path, 'html_files', 'Your Message Has Been Sent to Let MO Play.html')),
         email_str,
         'letmoplay@letmoplay.com',
         url_str='welcome_email',  # this needs to be updated
